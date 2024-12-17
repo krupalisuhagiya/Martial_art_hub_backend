30b5e71df721386c26d453bd9a6b2a6f63acba67
krupali <krupalisuhagiya2@gmail.com>
2024-12-17 17:12:23 +0530

    Upload 66

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 require("dotenv").config();
 const express = require("express");
-const cros = require("cors");
+const cors = require("cors");
 const mongoose = require("mongoose");
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
 
-app.use(cros());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // --------------------image live show -------------
+// Route for serving images
 app.get("/:folder1/:folder2/:folder3/:imagename", (req, res) => {
   const { folder1, folder2, folder3, imagename } = req.params;
-  console.log(req.params);
   const filePath = path.join(__dirname, folder1, folder2, folder3, imagename);
-  console.log(filePath);
   res.sendFile(filePath, (err) => {
     if (err) {
       res.status(500).send("Error displaying image");
@@ -27,23 +26,25 @@ app.get("/:folder1/:folder2/:folder3/:imagename", (req, res) => {
 });
 // -------------------------
 
-
+// Routes
 const student = require("./routes/student_route");
 const instructor = require("./routes/instructor_route");
 const errHandler = require("./error/error");
 
-app.use('/api',student);
-app.use('/api',instructor);
+app.use('/api', student);
+app.use('/api', instructor);
 
+// Error handler
 app.use(errHandler);
-mongoose
-  .connect(process.env.MO_URL)
+
+// Connect to MongoDB
+mongoose.connect(process.env.MO_URL)
   .then(() => {
     console.log("Database Connected!");
 
-    app.listen(4000, () => {
-      console.log("Server was running...");
-    });
+    app.listen(4000,() => {
+      console.log("Srever was runnig....")
+    })
   })
   .catch((error) => {
     console.log(error);
